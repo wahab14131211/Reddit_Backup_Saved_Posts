a4492bdd5d6d6e35d8773f1628dfe9f3a1069fd2
wahab14131211 <wahab14131211@users.noreply.github.com>
2022-11-13 22:28:11 -0500

    Add Functions to parse and extract submission and comment data from the reddit API. Also created a txt file with SQL commands to re-create the database which will be used to store this information

--- Reddit_Backup_Saved_Posts.py
+++ Reddit_Backup_Saved_Posts.py
@@ -1,6 +1,7 @@
 #Import Modules
-import json, praw, sys, os
+import json, praw, sys, os, inspect
 import datetime as datetime
+from pprint import pprint
 
 
 def load_reddit_cred_file():
@@ -26,6 +27,56 @@ def load_reddit_cred_file():
     print("Reddit credential file loaded successfully.\n", file=sys.stderr)
     return creds
 
+def parse_submission_type_post(dict,post):
+    if post.id is None or post.title is None or post.url is None:
+        print(f"The post {post} does not contain a valid id or url content. Skipping...\n")
+        return 1
+
+    if post.selftext == '[deleted]':
+        print(f"The post {post} was deleted. Skipping...\n")
+        return 1
+
+    if post.id in dict:
+        print(f"The id {saved_post.id} already exists in the dict. This id should be unique. Please file a bug on the Github on this issue.\n")
+        return 1
+
+    dict[post.id]={}
+    dict[post.id]['PostType'] = 'Submission'
+    dict[post.id]['DateTime'] = datetime.date.fromtimestamp(post.created_utc).isoformat()
+    dict[post.id]['Author'] = "Unknown"
+    dict[post.id]['Title'] = post.title
+    dict[post.id]['URL'] = post.url
+    dict[post.id]['Is_Self'] = post.is_self
+    dict[post.id]['Self_Text'] = post.selftext_html
+    dict[post.id]['Subreddit'] = post.subreddit.display_name
+    dict[post.id]['Permalink'] = post.permalink
+    dict[post.id]['Domain'] = post.domain
+    if post.author is not None:
+        dict[post.id]['Author'] = post.author.name
+    return 0
+
+def parse_comment_type_post(dict,comment):
+    if comment.id is None:
+        print(f"The comment {comment} does not contain a valid id. Skipping...\n")
+        return 1
+
+    if comment.id in dict:
+        print(f"The id {saved_comment.id} already exists in the dict. This id should be unique. Please file a bug on the Github on this issue.\n")
+        return 1
+
+    dict[comment.id] = {}
+    dict[comment.id]['PostType'] = 'Comment'
+    dict[comment.id]['Body'] = comment.body_html
+    dict[comment.id]['DateTime'] = datetime.date.fromtimestamp(comment.created_utc).isoformat()
+    dict[comment.id]['Permalink'] = comment.permalink
+    dict[comment.id]['LinkTitle'] = comment.link_title
+    dict[comment.id]['LinkUrl'] = comment.link_url
+    dict[comment.id]['Subreddit'] = comment.subreddit.display_name
+    if comment.author is not None:
+        dict[comment.id]['Author'] = comment.author.name
+    return 0
+
+
 def main():
     reddit_creds = load_reddit_cred_file()
     reddit = praw.Reddit(
@@ -42,6 +93,23 @@ def main():
         return 1
 
     print(f"Reddit username is {reddit_user.name}\n", file=sys.stderr)
+
+    saved_post_dict=dict()
+    for saved_post in reddit_user.saved(limit=None):
+        if type(saved_post).__name__ == 'Submission':
+            parse_submission_type_post(saved_post_dict,saved_post)
+        elif type(saved_post).__name__ == 'Comment':
+            parse_comment_type_post(saved_post_dict,saved_post)
+        else:
+            print("The following post type is not supported by this script. Please file a bug on the Github for an update.\n")
+
+    pprint(saved_post_dict)
+
+#        if saved_post.id not in test_list:
+#            print(f"NEW: Post ID is: {saved_post.id}\n")
+#            test_list.append(saved_post.id)
+#        else:
+#            print(f"DUPLICATED: post ID is: {saved_post.id}, Post creation datetime is: {datetime.date.fromtimestamp(saved_post.created_utc)}, OP is {saved_post.author.name}, Title is {saved_post.title}\n")
 
 if __name__ == "__main__":
     sys.exit(main())
